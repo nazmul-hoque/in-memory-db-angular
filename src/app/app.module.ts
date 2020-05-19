fa256d4a8950a5f5c3ec50d7de34fc3207912335
Build Manager <buildmanager@procom.ca>
2020-05-19 04:28:31 -0400

    Initial commint for this repo

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
+import { HttpClientModule} from '@angular/common/http';
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+
+import { AppComponent }  from './app.component';
+import { ObservableComponent }  from './observable.component';
+import { PromiseComponent }  from './promise.component';
+import { BookService } from './book.service';
+import { BookData } from './book-data';
+
+@NgModule({
+  imports: [     
+        BrowserModule,
+		FormsModule,
+            HttpModule,
+            HttpClientModule,
+		InMemoryWebApiModule.forRoot(BookData)
+  ],
+  declarations: [
+        AppComponent,
+		ObservableComponent,
+		PromiseComponent
+  ],
+  providers: [
+        BookService
+  ],
+  bootstrap: [
+        AppComponent
+  ]
+})
+export class AppModule { }
